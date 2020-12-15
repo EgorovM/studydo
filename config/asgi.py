b93e954c50968f6e4f5bd5aa2ed94d9acb67c650
EgorovM <EgorovM@bitbucket.org>
2020-12-15 20:47:14 +0900

    change asgi file

--- config/asgi.py
+++ config/asgi.py
@@ -1,11 +1,11 @@
 import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import room.routing
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 
 application = ProtocolTypeRouter({
